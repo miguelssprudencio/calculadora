d22ea6c865d55e5a4c85ae2bad2b47365c21538e
miguelssprudencio <miguel_s_prudencio@estudante.sesisenai.org.br>
2026-04-26 20:58:34 -0300

    Criei uma calculadora!

---
+++ calculator.js
@@ -0,0 +1,51 @@
+window.addEventListener('load', function () {
+    const calculator = new Calculator();
+});
+
+function Calculator() {
+    this.display = document.getElementById('display');
+    this.buttons = document.querySelectorAll('.button');
+  
+
+    this.buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            this.handleButtonClick(button);
+        });
+    });
+}
+
+Calculator.prototype.handleButtonClick = function (button) {
+    const value = button.textContent;
+
+    if (value === 'C') {
+        this.clearDisplay();
+    } else if (value === '=') {
+        this.calculateResult();
+    } else {
+        this.appendToDisplay(value);
+    }
+};
+
+Calculator.prototype.clearDisplay = function () {
+    this.display.value = '';
+};
+
+Calculator.prototype.appendToDisplay = function (value) {
+  
+    if (this.display.value === '0' && value !== '.') {
+        this.display.value = value;
+    } else {
+        this.display.value += value;
+    }
+};
+
+Calculator.prototype.calculateResult = function () {
+    try {
+        
+        const result = eval(this.display.value);
+        this.display.value = result;
+    } catch (error) {
+        this.display.value = 'Erro';
+        setTimeout(() => this.clearDisplay(), 1500);
+    }
+};
